23b14c2a00918eff87ab148812688db320f35093
ahmedmar3y2017 <ahmedmar3y108@gmail.com>
2017-11-20 16:43:33 +0200

    init

---
+++ src/beans/ImagesView.java
@@ -0,0 +1,26 @@
+package beans;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ImagesView {
+
+	private List<String> images;
+
+	@PostConstruct
+	public void init() {
+		images = new ArrayList<String>();
+		for (int i = 1; i <= 3; i++) {
+			images.add("a" + i + ".png");
+		}
+
+		System.out.println(images);
+	}
+
+	public List<String> getImages() {
+		return images;
+	}
+}
---
+++ src/beans/MenuView.java
@@ -0,0 +1,24 @@
+package beans;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+public class MenuView {
+	@PostConstruct
+	public void init() {
+
+	}
+
+	public void save() {
+		addMessage("Success", "Data saved");
+	}
+
+	public void addMessage(String summary, String detail) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+}
