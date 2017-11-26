b11bd28d7f8d16c6010fbedd49877f01d8b3e56a
ahmedmar3y2017 <ahmedmar3y108@gmail.com>
2017-11-27 01:36:23 +0200

    init

---
+++ src/beans/CeaserBean.java
@@ -0,0 +1,52 @@
+package beans;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+public class CeaserBean {
+
+	public int key;
+	public String plain;
+	public String cypher;
+
+	@PostConstruct
+	private void init() {
+
+	}
+
+	public String getCypher() {
+		return cypher;
+	}
+
+	public void setCypher(String cypher) {
+		this.cypher = cypher;
+	}
+
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	public String getPlain() {
+		return plain;
+	}
+
+	public void setPlain(String plain) {
+		this.plain = plain;
+	}
+
+	public void test() {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cypher is : " + plain));
+	}
+
+	public void test_dec() {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Plain is : " + cypher));
+	}
+
+}
